17a5fa178087a10c1cc125836b2c5e793d9ac7bf
Ujjwal Gupta <ujjwalkumargupta4u@gmail.com>
2022-02-03 22:07:18 +0530

    update template with logo

--- templates/typescript/src/index.ts
+++ templates/typescript/src/index.ts
@@ -1,5 +1,12 @@
 import { Mahal } from "mahal";
 import App from "@/app.mahal";
+import { registerGlobalFormatter } from "@/formatters";
+import config from "~/config";
 
 
-const app = new Mahal(App, '#app').create();
+const app = new Mahal(App, '#app');
+// register global formatter
+registerGlobalFormatter(app);
+// set config to be available globally
+app.global.config = config;
+app.create();
